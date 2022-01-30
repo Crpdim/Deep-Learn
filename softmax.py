5ab8d93104d5a9666904f0a346ef51d1cf610a9e
Crpdim <68361516+Crpdim@users.noreply.github.com>
2022-01-30 19:05:34 +0800

    submit

---
+++ softmax.py
@@ -0,0 +1,44 @@
+from http.client import ImproperConnectionState
+from turtle import down
+import torch
+import torchvision
+from torch.utils import data
+from torchvision import transforms
+from d2l import torch as d2l
+d2l.use_svg_display()
+
+trans = transforms.ToTensor()
+#下载训练数据集
+mnist_train = torchvision.datasets.FashionMNIST(root = "/Users/crpdim/Desktop/Deep-Learn/data",
+                            train = True,transform=trans,download=True)
+#下载测试数据集，用来验证模型好坏
+mnist_test = torchvision.datasets.FashionMNIST(root = "/Users/crpdim/Desktop/Deep-Learn/data",train = False,
+                                    transform=trans,download=True)
+
+print(mnist_train[0][0].shape)
+def get_fashion_mnist_labels(labels):  #@save
+    """返回Fashion-MNIST数据集的文本标签"""
+    text_labels = ['t-shirt', 'trouser', 'pullover', 'dress', 'coat',
+                   'sandal', 'shirt', 'sneaker', 'bag', 'ankle boot']
+    return [text_labels[int(i)] for i in labels]
+
+def show_images(imgs, num_rows, num_cols, titles=None, scale=1.5):  #@save
+    """绘制图像列表"""
+    figsize = (num_cols * scale, num_rows * scale)
+    _, axes = d2l.plt.subplots(num_rows, num_cols, figsize=figsize)
+    axes = axes.flatten()
+    for i, (ax, img) in enumerate(zip(axes, imgs)):
+        if torch.is_tensor(img):
+            # 图片张量
+            ax.imshow(img.numpy())
+        else:
+            # PIL图片
+            ax.imshow(img)
+        ax.axes.get_xaxis().set_visible(False)
+        ax.axes.get_yaxis().set_visible(False)
+        if titles:
+            ax.set_title(titles[i])
+    return axes
+
+X, y = next(iter(data.DataLoader(mnist_train, batch_size=18)))
+show_images(X.reshape(18, 28, 28), 2, 9, titles=get_fashion_mnist_labels(y));
