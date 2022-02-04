f797b379f310e12d6e06ff4a589405d974fb0540
Crpdim <68361516+Crpdim@users.noreply.github.com>
2022-02-04 20:14:29 +0800

    Update softmax.py

--- softmax.py
+++ softmax.py
@@ -1,4 +1,5 @@
 from http.client import ImproperConnectionState
+from logging import root
 from turtle import down
 import torch
 import torchvision
@@ -42,3 +43,33 @@ def show_images(imgs, num_rows, num_cols, titles=None, scale=1.5):  #@save
 
 X, y = next(iter(data.DataLoader(mnist_train, batch_size=18)))
 show_images(X.reshape(18, 28, 28), 2, 9, titles=get_fashion_mnist_labels(y));
+batch_size = 256
+
+def get_dataloader_workers():
+    #使用四个进程读取数据,加快读取速度
+    return 4
+train_iter = data.DataLoader(mnist_train,batch_size,shuffle=True,
+            num_workers=get_dataloader_workers())
+        
+timer = d2l.Timer()
+for X,y in train_iter:
+    continue
+print(f'{timer.stop():.2f} sec')
+def load_data_fashion_mnist(batch_size, resize=None):  #@save
+    """下载Fashion-MNIST数据集，然后将其加载到内存中"""
+    trans = [transforms.ToTensor()]
+    if resize:
+        trans.insert(0, transforms.Resize(resize))
+    trans = transforms.Compose(trans)
+    mnist_train = torchvision.datasets.FashionMNIST(
+        root="../data", train=True, transform=trans, download=True)
+    mnist_test = torchvision.datasets.FashionMNIST(
+        root="../data", train=False, transform=trans, download=True)
+    return (data.DataLoader(mnist_train, batch_size, shuffle=True,
+                            num_workers=get_dataloader_workers()),
+            data.DataLoader(mnist_test, batch_size, shuffle=False,
+                            num_workers=get_dataloader_workers()))
+
+
+#从零开始softmax
+ 
